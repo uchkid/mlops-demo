24f59a36343422b84040cab45344f51c461c1782
uchkid <uchkidoski@gmail.com>
2025-01-22 23:56:41 +0000

    first commit

---
+++ src/diabetes-training.py
@@ -0,0 +1,34 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+
+# load the diabetes dataset
+print("Loading Data...")
+diabetes = pd.read_csv('diabetes.csv')
+
+# separate features and labels
+X, y = diabetes[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, diabetes['Diabetic'].values
+
+# split data into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+# set regularization hyperparameter
+reg = 0.01
+
+# train a logistic regression model
+print('Training a logistic regression model with regularization rate of', reg)
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+# calculate accuracy
+y_hat = model.predict(X_test)
+acc = np.average(y_hat == y_test)
+print('Accuracy:', acc)
+
+# calculate AUC
+y_scores = model.predict_proba(X_test)
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('AUC: ' + str(auc))
